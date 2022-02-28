a9ef52af28de0beb4373942d554c2993e293c7a0
yusufaltattan <yusufaltattan@gmail.com>
2022-02-28 18:58:18 +0000

    commit

---
+++ capturestreatham/capture/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import get_object_or_404, render
+
+# Create your views here.
+from .models import Question
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'capture/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'capture/detail.html', {'question': question})
